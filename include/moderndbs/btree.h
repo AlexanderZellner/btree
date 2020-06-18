80b9eedab5fd3e0f9f954856d1631b6f13867497
Alexander Zellner <alexander.zellner@tum.de>
2020-06-18 14:16:19 +0200

    implement inner node

--- include/moderndbs/btree.h
+++ include/moderndbs/btree.h
@@ -31,12 +31,12 @@ struct BTree : public Segment {
     struct InnerNode: public Node {
         /// The capacity of a node.
         /// TODO think about the capacity that the nodes have
-        static constexpr uint32_t kCapacity = 42;
+        static constexpr uint32_t kCapacity = PageSize / sizeof(KeyT) + sizeof(ValueT) - 2;
 
         /// The keys.
         KeyT keys[kCapacity];
         /// The values.
-        uint64_t children[kCapacity];
+        uint64_t children[kCapacity + 1];
 
         /// Constructor.
         InnerNode() : Node(0, 0) {}
@@ -48,32 +48,116 @@ struct BTree : public Segment {
 
         /// Get the index of the first key that is not less than than a provided key.
         /// @param[in] key          The key that should be inserted.
-        std::pair<uint32_t, bool> lower_bound(const KeyT&) {
-            throw std::logic_error{"not implemented"};
+        std::pair<uint32_t, bool> lower_bound(const KeyT& keyT) {
+            // Binary search
+            std::pair<uint32_t, bool> result;
+            int first = 0;
+            // length of arrray
+            int last = this->count - 1;
+            std::optional<uint32_t> index = {};
+            assert(last < kCapacity);
+            if (last == 1 && keys[first] > keyT) {
+                result.first = first;
+                result.second = true;
+                return result;
+            }
+
+            while (first <= last) {
+                int middle = first + (last - first) / 2;
+
+                if (keyT == keys[middle]) {
+                    result.first = middle;
+                    result.second = true;
+                    return result;
+                }
+
+                if (keyT > keys[middle]) {
+                    first = middle + 1;
+                } else {
+                    index = middle;
+                    last = middle - 1;
+                }
+            }
+
+            if(index) {
+                result.first = index.value();
+                result.second = true;
+            } else {
+                result.first = 0;
+                result.second = false;
+            }
+            return result;
         }
 
         /// Insert a key.
         /// @param[in] key          The key that should be inserted.
         /// @param[in] separator    The child that should be inserted.
-        void insert(const KeyT&, uint64_t) {
-            throw std::logic_error{"not implemented"};
+        void insert(const KeyT& keyT, uint64_t child) {
+            // insert key in keys[] -> insert child in children (page id)
+            auto lower_bound = this->lower_bound(keyT);
+            //vectorify arrays to insert key and child at correct point
+            std::vector<KeyT> keys_vector = get_key_vector();
+            std::vector<uint64_t> children_vector = get_child_vector();
+
+            if (lower_bound.second) {
+                keys_vector.insert(keys_vector.begin() + lower_bound.first, keyT);
+                /// TODO: Check tree side
+                children_vector.insert(children_vector.begin() + lower_bound.first, child);
+            } else {
+                // keyT is greater than all existing keys
+                /// TODO: check split needed
+                keys_vector.push_back(keyT);
+                children_vector.push_back(child);
+            }
+            this->count++;
+
+            // Copy vectos back to array
+            keys = keys_vector.data();
+            children = children_vector.data();
+
+            assert(keys[lower_bound] == keys_vector.at(lower_bound));
+            assert(children[lower_bound] == children_vector.at(lower_bound));
         }
 
         /// Split the node.
         /// @param[in] buffer       The buffer for the new page.
         /// @return                 The separator key.
-        KeyT split(std::byte*) {
-            throw std::logic_error{"not implemented"};
+        KeyT split(std::byte* buffer) {
+            // node must be full
+            assert(*(&keys + 1) - keys == kCapacity);
+            auto middle = (kCapacity - 1)  / 2;
+            InnerNode* new_inner_node = reinterpret_cast<InnerNode*>(buffer);
+
+            // get seperator key
+            KeyT seperator_key = keys[middle];
+
+            KeyT tmp_keys[kCapacity];
+            uint64_t tmp_children[kCapacity + 1];
+            // copy one half to new node
+            for (int j = middle; j < kCapacity; ++j) {
+                tmp_keys = keys[j];
+                tmp_children = children[j];
+                keys[j] = {};
+                children[j] = {};
+            }
+
+            new_inner_node->keys = tmp_keys;
+            new_inner_node->children = tmp_children;
+            
+            return seperator_key;
         }
 
         /// Returns the keys.
         std::vector<KeyT> get_key_vector() {
-            throw std::logic_error{"not implemented"};
+            std::vector<KeyT> keys_vector(std::begin(keys), std::end(keys));
+            return keys_vector;
+
         }
 
         /// Returns the child page ids.
         std::vector<uint64_t> get_child_vector() {
-            throw std::logic_error{"not implemented"};
+            std::vector<uint64_t> children_vector(std::begin(children), std::end(children));
+            return children_vector;
         }
     };
 
